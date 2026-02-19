a8040e2403ed39e5af9d3e076e5bbb8c353110c7
HAB712 <habibamursaleen20@gmail.com>
2026-02-20 02:57:38 +0500

    add docker file

--- RadioCab/obj/Debug/net8.0/RadioCab.AssemblyInfo.cs
+++ RadioCab/obj/Debug/net8.0/RadioCab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RadioCab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a892d448a328940ddc66c7cecb1a5fd8172765fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RadioCab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RadioCab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
